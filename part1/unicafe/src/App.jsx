4ac690b37a9ded97b7ae1ba8d819df69d9f6c168
Koskelainen Sara <Sara.Koskelainen@syke.fi>
2025-09-06 15:58:29 +0300

    Add stats to unicafe feedback app (ex 1.7)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,14 +5,50 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
+  const [avg, setAvg] = useState(0);
 
   // feedback event handlers
-  const handleGoodFeedBack = () => setGood(good + 1);
-  const handleNeutralFeedBack = () => setNeutral(neutral + 1);
-  const handleBadFeedBack = () => setBad(bad + 1);
+  const handleGoodFeedBack = () => {
+    const newGood = good + 1;
+    setGood(newGood);
+    const newAll = newGood + neutral + bad;
+    setAll(newAll); // Could also be all + 1. maybe the way used here would be safer if operation would be complex, not just +1 (?) & makes below calculations clean
+    setPositivePercentage((newGood / newAll) * 100);
+    calculateAndSetAvg(newGood, bad, newAll);
+  };
+  const handleNeutralFeedBack = () => {
+    setNeutral(neutral + 1);
+    const newAll = all + 1;
+    setAll(newAll);
+    setPositivePercentage((good / newAll) * 100);
+    calculateAndSetAvg(good, bad, newAll);
+  };
+  const handleBadFeedBack = () => {
+    setBad(bad + 1);
+    const newAll = all + 1;
+    setAll(newAll);
+    setPositivePercentage((good / newAll) * 100);
+    calculateAndSetAvg(good, bad + 1, newAll);
+  };
+
+  const calculateAndSetAvg = (countGood, countBad, countAll) => {
+    // bc the values for feedbacks are 1 (good), 0 (neutral), and -1 (bad), the calculation
+    // (countGood*1 + countNeutal*0 + countBad*-1) / countAll
+    // can be simplified to what is used below
+    const newAvg = (countGood - countBad) / countAll;
+    setAvg(newAvg);
+  };
 
   const Button = ({ onClick, text }) => (
     <button onClick={onClick}>{text}</button>
+  );
+
+  const StatsContent = ({ text, value, unit = "" }) => (
+    <p>
+      {text} {value} {unit}
+    </p>
   );
 
   return (
@@ -24,9 +60,12 @@ const App = () => {
       <Button onClick={handleBadFeedBack} text="bad" />
 
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <StatsContent text={"good"} value={good} />
+      <StatsContent text={"neutral"} value={neutral} />
+      <StatsContent text={"bad"} value={bad} />
+      <StatsContent text={"all"} value={all} />
+      <StatsContent text={"average"} value={avg} />
+      <StatsContent text={"positive"} value={positivePercentage} unit={"%"} />
     </div>
   );
 };
