e83b27428f83a79e6a7473e57a8d9a889db43cf3
Koskelainen Sara <Sara.Koskelainen@syke.fi>
2025-09-06 20:25:20 +0300

    Update anecdotes app to show most voted anectode, ex. 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
     "The only way to go fast, is to go well.",
   ];
 
+  // Random selection of anactode
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
@@ -23,7 +24,7 @@ const App = () => {
     setSelected(getRandomInt(maxIndex));
   };
 
-  // create zero filled array of length 8
+  // Voting of anectodes
   const initialVotes = new Array(anecdotes.length).fill(0);
 
   const [votes, setVotes] = useState(initialVotes);
@@ -32,14 +33,35 @@ const App = () => {
     let newVotes = [...votes];
     newVotes[selected] += 1;
     setVotes(newVotes);
+
+    // get and set the index of the most voted anectode
+    const maxVotes = Math.max(...newVotes);
+    const mostVotedIndex = newVotes.indexOf(maxVotes);
+    setIndexOfMostVoted(mostVotedIndex);
   };
+
+  // Most voted anectode
+  const [indexOfMostVoted, setIndexOfMostVoted] = useState(-1);
 
   return (
     <div>
+      <h1>Anectode of the day</h1>
       <div>{anecdotes[selected]}</div>
       <div> has {votes[selected]} votes</div>
       <button onClick={handleVote}>vote</button>
       <button onClick={handleNext}>next anecdote</button>
+
+      <h1>Anectode with most votes</h1>
+
+      {/* Note: If multiple anecdotes are tied for the first place, only the first of them is showed */}
+      {indexOfMostVoted >= 0 ? (
+        <div>
+          <div> {anecdotes[indexOfMostVoted]}</div>
+          <div> has {votes[indexOfMostVoted]} votes</div>
+        </div>
+      ) : (
+        <div>No voted anecdotes</div>
+      )}
     </div>
   );
 };
