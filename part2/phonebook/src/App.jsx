cc12f94c8dc9b0bb31f180cc933f02e6a6ab1d7c
Sara Koskelainen <koskelainen.sara@gmail.com>
2025-10-19 20:29:04 +0300

    add number input to phonebook app, ex. 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -20,11 +22,14 @@ const App = () => {
           name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <p key={p.name}> {p.name} </p>)}
+      {persons.map(p => <p key={p.name}> {p.name} {p.number}</p>)}
     </div>
   )
 }
